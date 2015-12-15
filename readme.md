a2429b3f355448eb91763828fb85df57fba76c18
Mark Bjerke <markb@iship.com>
2015-12-15 11:30:50 -0800

    # This is a combination of 2 commits. # The first commit's message is:
    Implement no-loop rule attribute
    
    Fixes issue with using grave marks for dsl strings in test file: noloop.test.js
    
    Implements extends functionality in the dsl
    
    These files address #162 , the extends keyword, which allows you to
    reference other types in either external or other defines as a base
    type.
    
    Revert "Implements extends functionality in the dsl"
    
    This reverts commit 1259c3d5c4779e9e425dfb9db219c700bf12e92f.
    
    removed gitattributs/gitignore from branch
    
    # This is the 2nd commit message:
    
    Fixes issue with using grave marks for dsl strings in test file: noloop.test.js

--- readme.md
+++ readme.md
@@ -37,6 +37,7 @@ Or [download the source](https://raw.github.com/C2FO/nools/master/nools.js) ([mi
       * [Structure](#rule-structure)
       * [Salience](#rule-salience)
       * [Scope](#rule-scope)
+	  * [no-loop](#rule-no-loop)
       * [Constraints](#constraints)
         * [Not](#not-constraint)
         * [Or](#or-constraint)
@@ -999,6 +1000,33 @@ flow1
     .then(function(){
         console.log(fired); //["Hello1", "Hello2", "Hello3", "Hello4"]
     });
+```
+
+<a name="rule-no-loop"></a>
+### No-Loop
+
+When a rule's action modifies a fact it may cause the rule to activate again, causing an infinite loop. Setting no-loop to true will skip the creation of another Activation for the rule with the current set of facts.
+
+```javascript
+this.rule("Hello", {noLoop: true}, [Message, "m", "m.text like /hello/"], function (facts) {
+	var m = facts.m;
+	m.text = 'hello world';    
+	this.modify(m)
+});
+
+```
+Or using the DSL
+
+```javascript
+rule Hello {
+    no-loop: true;
+    when {
+        m: Message m.name like /hello/;
+    }
+    then { modify(m, function() {
+		m.text = 'hello world'
+	});
+}
 ```
 
 
---
+++ test/flow/noLoop.test.js
@@ -0,0 +1,67 @@
+﻿"use strict";
+var it = require("it"),
+    assert = require("assert"),
+    nools = require("../../");
+
+it.describe("no-loop", function (it) {
+    /*jshint indent*/
+    function Message(name) {
+        this.name = name;
+    }
+    var cnt = 0;   
+    
+    var flow1 = nools.flow("noLoop1", function () {
+        
+        this.rule("Hello2", { noLoop: true }, [Message, "m", "m.name =~ /Hello/"], function (facts) {
+            var m = facts.m;
+            m.name = 'Hello World';
+            this.modify(m);
+        });
+    }),
+	
+        flow2 = nools.flow("noLoop2", function () {
+            
+            this.rule("Hello1", [Message, "m", "m.name =~ /Hello/"], function (facts) {
+                var m = facts.m;
+                if (cnt++ < 2) {
+                    m.name = 'Hello World';
+                    this.modify(m);
+                }
+            });
+        });
+
+    var noolsSource =  "rule 'Hello3' { no-loop: true; when {m: Message m.name =~/Hello/;}then {modify(m, function () { this.name = 'Hello World'; });}}";
+
+    var flow3 = nools.compile(noolsSource, {
+        name: 'testDsl'
+        ,define: {
+            Message: Message
+        }
+    });
+   
+    it.should("not loop with option on and loop otherwise", function () {
+        var fired1 = [], fired2 = [], fired3 = [];
+        var session1 = flow1.getSession(new Message("Hello")).on("fire", function (name) {
+            fired1.push(name);
+        }),
+        session2 = flow2.getSession(new Message("Hello")).on("fire", function (name) {
+            fired2.push(name);
+        }),
+        session3 = flow3.getSession(new Message("Hello")).on("fire", function (name) {
+            fired3.push(name);
+        });
+        return session1.match()
+            .then(function () {
+        return session2.match().then(function () {
+            return session3.match().then(function () {
+            })
+        })
+        })
+       .then(function () {
+            assert.deepEqual(fired1, ["Hello2"]);
+            assert.deepEqual(fired2, ["Hello1", "Hello1", "Hello1"]);
+            assert.deepEqual(fired3, ["Hello3"]);
+        });
+    });
+
+});
